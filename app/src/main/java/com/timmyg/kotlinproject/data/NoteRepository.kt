e20b18b5a9066eab8c0b3c8ec32b329b86d01de4
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-02 19:33:22 +0300

    NoteRepository without get & init

--- app/src/main/java/com/timmyg/kotlinproject/data/NoteRepository.kt
+++ app/src/main/java/com/timmyg/kotlinproject/data/NoteRepository.kt
@@ -4,16 +4,13 @@ import android.graphics.Color
 import com.timmyg.kotlinproject.data.entity.Note
 
 object NoteRepository {
-    private val notes: List<Note>
+    private val notes: List<Note> = listOf(Note("Заметка1", "Текст заметки 1", Color.BLUE),
+            Note("Заметка2", "Текст заметки 2", Color.RED),
+            Note("Заметка3", "Текст заметки 3", Color.GREEN),
+            Note("Заметка4", "Текст заметки 4", Color.YELLOW),
+            Note("Заметка5", "Текст заметки 5", Color.GRAY),
+            Note("Заметка6", "Текст заметки 6", Color.MAGENTA)
+    )
 
-    init{
-        notes = listOf(Note("Заметка1", "Текст заметки 1", Color.BLUE),
-                Note("Заметка2", "Текст заметки 2", Color.RED),
-                Note("Заметка3", "Текст заметки 3", Color.GREEN),
-                Note("Заметка4", "Текст заметки 4", Color.YELLOW),
-                Note("Заметка5", "Текст заметки 5", Color.GRAY),
-                Note("Заметка6", "Текст заметки 6", Color.MAGENTA)
-                )
-    }
 
 }
