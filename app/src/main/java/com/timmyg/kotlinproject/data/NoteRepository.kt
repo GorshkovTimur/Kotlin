9d82587b3f2d5f06e5995cbecf90d030a8eec3cf
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-05 06:48:28 +0300

    add functions & some fixes

--- app/src/main/java/com/timmyg/kotlinproject/data/NoteRepository.kt
+++ app/src/main/java/com/timmyg/kotlinproject/data/NoteRepository.kt
@@ -1,11 +1,16 @@
 package com.timmyg.kotlinproject.data
 
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.timmyg.kotlinproject.data.entity.Note
 import java.util.*
 
 object NoteRepository {
-     val notes: List<Note> = listOf(Note(UUID.randomUUID().toString(), "Заметка1", "Текст заметки 1", Note.Color.BLUE),
+
+    private val notesLiveData  = MutableLiveData<List<Note>>()
+
+     var notes: MutableList<Note> = mutableListOf(Note(UUID.randomUUID().toString(), "Заметка1", "Текст заметки 1", Note.Color.BLUE),
             Note(UUID.randomUUID().toString(),"Заметка2", "Текст заметки 2", Note.Color.RED),
             Note(UUID.randomUUID().toString(),"Заметка3", "Текст заметки 3", Note.Color.GREEN),
             Note(UUID.randomUUID().toString(),"Заметка4", "Текст заметки 4", Note.Color.PINK),
@@ -13,5 +18,24 @@ object NoteRepository {
             Note(UUID.randomUUID().toString(),"Заметка6", "Текст заметки 6", Note.Color.VIOLET)
     )
 
+    init {
+        notesLiveData.value = notes
+    }
+
+    fun saveNote(note: Note){
+        addOrReplace(note)
+        notesLiveData.value = notes
+
+    }
+
+    private fun addOrReplace(note: Note){
+        for (i in notes.indices){
+            if (notes[i] == note){
+                notes[i] = note
+                return
+            }
+        }
+        notes.add(note)
+    }
 
 }
