5626bf4aee8cd31b8c3f119b918de23fd1dd573a
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-08 17:08:06 +0300

    MainAtivity done

--- app/src/main/java/com/timmyg/kotlinproject/ui/main/MainActivity.kt
+++ app/src/main/java/com/timmyg/kotlinproject/ui/main/MainActivity.kt
@@ -4,15 +4,22 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.timmyg.kotlinproject.ui.note.NoteActivity
 import com.timmyg.kotlinproject.R
+import com.timmyg.kotlinproject.data.entity.Note
+import com.timmyg.kotlinproject.ui.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<List<Note>? , MainViewState>() {
 
-    lateinit var viewModel: MainViewModel
+    override val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override val layoutRes = R.layout.activity_main
     lateinit var adapter: NotesRVAdapter
 
 
@@ -21,20 +28,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-
         recycler_view.layoutManager = GridLayoutManager(this, 2)
         adapter = NotesRVAdapter { note ->
             NoteActivity.start(this, note)
         }
         recycler_view.adapter = adapter
 
-        viewModel.viewState().observe(this, Observer{
-            it?.let {adapter.notes = it.notes}
-        })
-
         fab.setOnClickListener{
             NoteActivity.start(this)
+        }
+    }
+
+    override fun renderData(data: List<Note>?) {
+        data?.let {
+            adapter.notes = it
         }
     }
 }
--- app/src/main/java/com/timmyg/kotlinproject/ui/note/NoteViewModel.kt
+++ app/src/main/java/com/timmyg/kotlinproject/ui/note/NoteViewModel.kt
@@ -1,18 +1,44 @@
 package com.timmyg.kotlinproject.ui.note
 
-import androidx.lifecycle.ViewModel
+
+import androidx.lifecycle.Observer
 import com.timmyg.kotlinproject.data.NoteRepository
 import com.timmyg.kotlinproject.data.entity.Note
+import com.timmyg.kotlinproject.data.entity.NoteResult
+import com.timmyg.kotlinproject.ui.base.BaseViewModel
 
-class NoteViewModel:ViewModel() {
-    private var pendintNote:Note ?= null
+
+class NoteViewModel: BaseViewModel <Note?, NoteViewState>() {
+
+    init {
+        viewStateLiveData.value = NoteViewState()
+    }
+
+    private var pendingNote:Note? = null
 
     fun save(note: Note){
-        pendintNote = note
+        pendingNote = note
+    }
+
+    fun loadNote(noteId: String){
+        NoteRepository.getNoteById(noteId).observeForever(object : Observer<NoteResult>{
+            override fun onChanged(t: NoteResult?) {
+                t ?: return
+                when (t){
+                    is NoteResult.Succes<*> -> {
+                        viewStateLiveData.value = NoteViewState(note = t.data as? Note)
+                    }
+                    is NoteResult.Error -> {
+                        viewStateLiveData.value = NoteViewState(error =  t.error)
+                    }
+                }
+            }
+
+        })
     }
 
     override fun onCleared() {
-        pendintNote?.let {
+        pendingNote?.let {
             NoteRepository.saveNote(it)  }
         }
 
