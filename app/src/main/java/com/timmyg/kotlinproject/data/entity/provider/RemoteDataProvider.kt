0bf5ba9856daf4aa7411278c9cd352ebc912128d
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-15 14:25:23 +0300

    change signature

--- app/src/main/java/com/timmyg/kotlinproject/data/entity/provider/RemoteDataProvider.kt
+++ app/src/main/java/com/timmyg/kotlinproject/data/entity/provider/RemoteDataProvider.kt
@@ -1,13 +1,13 @@
 package com.timmyg.kotlinproject.data.entity.provider
 
 import androidx.lifecycle.LiveData
-import com.firebase.ui.auth.data.model.User
 import com.timmyg.kotlinproject.data.entity.Note
 import com.timmyg.kotlinproject.data.entity.NoteResult
+import com.timmyg.kotlinproject.data.entity.User
 
 interface RemoteDataProvider {
     fun subscribeToAllNotes() : LiveData<NoteResult>
     fun getNoteById(id: String) : LiveData<NoteResult>
     fun saveNote (note: Note) : LiveData<NoteResult>
-    fun getCurrentUser(): LiveData<User>
+    fun getCurrentUser(): LiveData<User?>
 }
