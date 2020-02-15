c28c2b0cbeecb2e796bee274cdbefc9fcdc49fd5
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-15 14:25:30 +0300

    add fun

--- app/src/main/java/com/timmyg/kotlinproject/data/NoteRepository.kt
+++ app/src/main/java/com/timmyg/kotlinproject/data/NoteRepository.kt
@@ -14,4 +14,5 @@ private val remoteProvider: RemoteDataProvider = FireStoreProvider()
     fun getNotes() = remoteProvider.subscribeToAllNotes()
     fun saveNote(note: Note) = remoteProvider.saveNote(note)
     fun getNoteById(id: String) = remoteProvider.getNoteById(id)
+    fun getCurrentUser() = remoteProvider.getCurrentUser()
 }
