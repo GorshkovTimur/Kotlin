52f32c297e455b8644427635ff7857daa4448db6
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-15 14:03:56 +0300

    add fun 2 interface

--- app/src/main/java/com/timmyg/kotlinproject/data/entity/provider/RemoteDataProvider.kt
+++ app/src/main/java/com/timmyg/kotlinproject/data/entity/provider/RemoteDataProvider.kt
@@ -1,6 +1,7 @@
 package com.timmyg.kotlinproject.data.entity.provider
 
 import androidx.lifecycle.LiveData
+import com.firebase.ui.auth.data.model.User
 import com.timmyg.kotlinproject.data.entity.Note
 import com.timmyg.kotlinproject.data.entity.NoteResult
 
@@ -8,4 +9,7 @@ interface RemoteDataProvider {
     fun subscribeToAllNotes() : LiveData<NoteResult>
     fun getNoteById(id: String) : LiveData<NoteResult>
     fun saveNote (note: Note) : LiveData<NoteResult>
+    fun getCurrentUser(): LiveData<User> {
+
+    }
 }
