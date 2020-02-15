73ff69487424039d037f96bb8d0a9fd73d7834f9
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-15 14:25:40 +0300

    some adds to provider

--- app/src/main/java/com/timmyg/kotlinproject/data/entity/provider/FireStoreProvider.kt
+++ app/src/main/java/com/timmyg/kotlinproject/data/entity/provider/FireStoreProvider.kt
@@ -1,26 +1,44 @@
 package com.timmyg.kotlinproject.data.entity.provider
 
-import androidx.lifecycle.LiveData
+
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.timmyg.kotlinproject.data.entity.Note
 import com.timmyg.kotlinproject.data.entity.NoteResult
+import com.timmyg.kotlinproject.data.entity.User
+import com.timmyg.kotlinproject.data.entity.errors.NoAuthException
 
 class FireStoreProvider: RemoteDataProvider {
 
         companion object{
             private const val NOTES_COLLECTION = "notes"
+            private const val USER_COLLECTION = "users"
         }
 
-    private val store = FirebaseFirestore.getInstance()
-    private val noteReference = store.collection(NOTES_COLLECTION)
+    private val store by lazy { FirebaseFirestore.getInstance()}
+
+    private val UserNoteCollection : CollectionReference
+    get() =
+        currentUser?.let {
+            store.collection(USER_COLLECTION).document(it.uid).collection(NOTES_COLLECTION)
+        } ?: throw NoAuthException()
 
 
 
+    private val currentUser
+            get() = FirebaseAuth.getInstance().currentUser
+
+
+    override fun getCurrentUser() = MutableLiveData<User?>().apply {
+        value = currentUser?.let { firebaseUser ->
+            User(firebaseUser.displayName?: "", firebaseUser.email?:"")
+        }
+    }
 
     override fun subscribeToAllNotes() = MutableLiveData<NoteResult>().apply {
-        noteReference.addSnapshotListener { snapshot, e ->
+        UserNoteCollection.addSnapshotListener { snapshot, e ->
             e?.let {
                value = NoteResult.Error(e)
             } ?: let {
@@ -33,7 +51,7 @@ class FireStoreProvider: RemoteDataProvider {
 
     override fun getNoteById(id: String)= MutableLiveData<NoteResult>().apply {
 
-        noteReference.document(id).get()
+        UserNoteCollection.document(id).get()
                 .addOnSuccessListener {snap->
                     value = NoteResult.Success(snap.toObject(Note::class.java))
                 }.addOnFailureListener{
@@ -42,7 +60,7 @@ class FireStoreProvider: RemoteDataProvider {
     }
 
     override fun saveNote(note: Note) = MutableLiveData<NoteResult>().apply {
-        noteReference.document(note.id).set(note)
+        UserNoteCollection.document(note.id).set(note)
                 .addOnSuccessListener {
                     value = NoteResult.Success(note)
                 }.addOnFailureListener{
