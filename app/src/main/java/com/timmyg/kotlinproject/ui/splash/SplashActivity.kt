f4cb51553575816c0f5fdc8804b376b921afb631
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-19 11:50:11 +0300

    rename viewModel

--- app/src/main/java/com/timmyg/kotlinproject/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/timmyg/kotlinproject/ui/splash/SplashActivity.kt
@@ -3,11 +3,10 @@ package com.timmyg.kotlinproject.ui.splash
 import android.os.Handler
 import androidx.lifecycle.ViewModelProvider
 import com.timmyg.kotlinproject.ui.base.BaseActivity
-import com.timmyg.kotlinproject.ui.base.BaseViewModel
 import com.timmyg.kotlinproject.ui.main.MainActivity
 
 class SplashActivity: BaseActivity<Boolean?, SplashViewState>() {
-    override val viewModel by lazy {
+    override val model by lazy {
         ViewModelProvider(this).get(SplashViewModel::class.java)
     }
 
@@ -15,7 +14,7 @@ class SplashActivity: BaseActivity<Boolean?, SplashViewState>() {
 
     override fun onResume() {
         super.onResume()
-        Handler().postDelayed({viewModel.requestUser()}, 1000)
+        Handler().postDelayed({model.requestUser()}, 1000)
     }
 
     override fun renderData(data: Boolean?) {
