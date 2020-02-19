3f1b4ee3471332fb0d4f91d730d205351bac91ac
GorshkovTimur <gorshkoff.timur@gmail.com>
2020-02-19 11:28:14 +0300

    add DI

--- app/src/main/java/com/timmyg/kotlinproject/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/timmyg/kotlinproject/ui/splash/SplashViewModel.kt
@@ -4,10 +4,10 @@ import com.timmyg.kotlinproject.data.NoteRepository
 import com.timmyg.kotlinproject.data.entity.errors.NoAuthException
 import com.timmyg.kotlinproject.ui.base.BaseViewModel
 
-class SplashViewModel:BaseViewModel<Boolean?, SplashViewState>() {
+class SplashViewModel(private val noteRepository: NoteRepository):BaseViewModel<Boolean?, SplashViewState>() {
 
     fun requestUser(){
-        NoteRepository.getCurrentUser().observeForever{
+        noteRepository.getCurrentUser().observeForever{
             viewStateLiveData.value = it?.let{
                 SplashViewState(authenticated = true)
             } ?: let { SplashViewState(error = NoAuthException()) }
